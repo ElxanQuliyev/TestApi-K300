1d327bf7350a05f623e3cbe70dd91bccd5e53f18
Elxan Quliyev <qelxan177@gmail.com>
2020-08-18 20:44:45 +0400

    Api

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added successfully";
+        public static string ProductDeleted = "Product deleted successfully";
+        public static string ProductUpdated = "Product updated successfully";
+        public static string UserNotFound = "User not found";
+        public static string PasswordError = "Password is not correct";
+        public static string SuccessfulLogin = "Success user login";
+        public static string UserAlreadyExists = "User already exists";
+        public static string UserRegistered = "User register successfully";
+
+
+        public static string AccessTokenCreated = "Access token create successfully";
+    }
+}
